5c533a8d9d80946684c7a50d69d538ff45c49e8d
Guillermo Granados <guillermo.granados@outlook.com>
2021-11-10 11:47:08 -0300

    refactored controller

--- secret-santa-service/src/main/kotlin/com/ggranados/sercretsanta/api/controller/PersonController.kt
+++ secret-santa-service/src/main/kotlin/com/ggranados/sercretsanta/api/controller/PersonController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/persons")
@@ -16,19 +17,41 @@ class PersonController {
     lateinit var  personServiceAPI: PersonaServiceImpl
 
     @GetMapping
-    fun getAll() : MutableList<Person>? {
+    fun getAllPersons() : MutableList<Person>? {
         return personServiceAPI.all
     }
 
     @PostMapping
-    fun save(@RequestBody person: Person) : ResponseEntity<Person>{
-        var obj = personServiceAPI.save(person);
-        return ResponseEntity<Person>(person, HttpStatus.OK)
+    fun savePerson(@RequestBody person: Person) : ResponseEntity<Person>{
+        var p = personServiceAPI.save(person);
+        return ResponseEntity<Person>(p, HttpStatus.OK)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePerson(@PathVariable id: Long, @RequestBody person: Person) : ResponseEntity<Person>{
+        var persona = personServiceAPI[id];
+        return if (Objects.nonNull(persona)) {
+            var p = personServiceAPI.save(person);
+            ResponseEntity<Person>(p,HttpStatus.OK);
+        }else{
+            ResponseEntity<Person>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{id}")
+    fun getPersona(@PathVariable id: Long) : ResponseEntity<Person>{
+
+        var persona = personServiceAPI[id];
+        return if (Objects.nonNull(persona)) {
+            ResponseEntity<Person>(persona,HttpStatus.OK);
+        }else{
+            ResponseEntity<Person>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long) : ResponseEntity<Person>{
-        return if (personServiceAPI.get(id) != null) {
+        return if (personServiceAPI[id] != null) {
             personServiceAPI.delete(id);
             ResponseEntity<Person>(HttpStatus.NO_CONTENT);
         }else{
