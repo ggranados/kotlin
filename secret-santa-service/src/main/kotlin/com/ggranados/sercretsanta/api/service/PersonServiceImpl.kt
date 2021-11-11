87e5547075c995937fe766f39b82cdac9a16add0
Guillermo Granados <guillermo.granados@outlook.com>
2021-11-10 23:30:15 -0300

    added assign function to service

--- secret-santa-service/src/main/kotlin/com/ggranados/sercretsanta/api/service/PersonServiceImpl.kt
+++ secret-santa-service/src/main/kotlin/com/ggranados/sercretsanta/api/service/PersonServiceImpl.kt
@@ -2,10 +2,15 @@ package com.ggranados.sercretsanta.api.service
 
 import com.ggranados.sercretsanta.api.common.GenericServiceImpl
 import com.ggranados.sercretsanta.api.model.Person
+import com.ggranados.sercretsanta.api.model.PersonUtils
 import com.ggranados.sercretsanta.api.repository.PersonRepository
+import javassist.NotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.CrudRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class PersonaServiceImpl : GenericServiceImpl<Person, Long>(), PersonaServiceAPI {
@@ -16,4 +21,15 @@ class PersonaServiceImpl : GenericServiceImpl<Person, Long>(), PersonaServiceAPI
     override val dao: CrudRepository<Person, Long>
         get() =  personaRepository
 
+    fun assign(idPerson: Long, idAssigned: Long): Person? {
+        val personRequested = this[idPerson];
+        val assignedRequested = this[idAssigned];
+
+        return if (Objects.nonNull(personRequested).and(Objects.nonNull(assignedRequested))) {
+            val assigned = PersonUtils.getPersonAssigned(assignedRequested!!);
+            val person = PersonUtils.getPersonWithAssignation(personRequested!!, assigned);
+            this.save(person);
+            person;
+        }else null
+    }
 }
