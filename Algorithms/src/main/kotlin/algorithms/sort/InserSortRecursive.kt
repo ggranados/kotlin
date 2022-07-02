c287ebedb7b3ab596cbceedda0e690c4217bbc9f
Guillermo Granados <guillermo.granados@outlook.com>
2022-07-01 23:36:39 -0400

    [insert sort recursive] added recursive

---
+++ Algorithms/src/main/kotlin/algorithms/sort/InserSortRecursive.kt
@@ -0,0 +1,35 @@
+package algorithms.sort
+
+class InsertSortRecursive {
+
+    fun sort(elements: MutableList<Int>): MutableList<Int> {
+
+        return sort(elements, 1)
+    }
+
+    private fun sort(elements: MutableList<Int>, index: Int): MutableList<Int> {
+
+        if (index >= elements.size)
+            return elements
+
+        val key = elements[index]
+        val prevIndex = index - 1
+
+        replaceBackwards(elements, key, prevIndex)
+
+        return sort(elements, index + 1)
+    }
+
+    private fun replaceBackwards(elements: MutableList<Int>, key: Int, prevIndex: Int) {
+        if(prevIndex < 0 || key >= elements[prevIndex])
+           return
+
+        elements[prevIndex + 1] = elements[prevIndex]
+        elements[prevIndex] = key
+
+        replaceBackwards(elements, key, prevIndex-1)
+
+    }
+
+
+}
---
+++ Algorithms/src/test/kotlin/algorithms/sort/InsertSortRecursiveTest.kt
@@ -0,0 +1,46 @@
+package algorithms.sort
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class InsertSortRecursiveKtTest {
+
+    val sorter: InsertSortRecursive = InsertSortRecursive()
+
+    @Test
+    fun testSort() {
+        //given
+        val elements = mutableListOf(3,8,6,9,6,0)
+
+        //when
+        print(sorter.sort(elements))
+
+        //then
+        val lastElement: Optional<Int> = elements.stream()
+            .reduce { a, b ->
+                assertTrue(
+                    isEqualOrGreaterThan(b, a),
+                    "$b element is not equal or greater than previous $a"
+                )
+                b
+            }
+
+        val max = elements.stream()
+            .mapToInt { v: Int? -> v!! }.max()
+
+        assertTrue(lastElement.isPresent, "last element must be present")
+        assertTrue(max.isPresent, "max element must be present")
+
+        assertEquals(
+            max.asInt,
+            lastElement.get(),
+            "Last element should be the max value"
+        )
+    }
+
+    private fun isEqualOrGreaterThan(next: Int, prev: Int): Boolean {
+        return next >= prev
+    }
+
+}
